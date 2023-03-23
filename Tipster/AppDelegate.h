e3ad0dd41511407f93dffd04a4051d44813bac9e
JPL11 <jackydli95@gmail.com>
2023-03-23 11:24:22 -0700

    Initial Commit

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by Jacky Li on 3/23/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Tipster/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by Jacky Li on 3/23/23.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
